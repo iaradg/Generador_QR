657d04afbf5af7199a6a51ccc85f9c8073efbc6b
iara dg <iaradg123@gmail.com>
2022-11-07 21:52:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lector_generador_qr/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.lector_generador_qr
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var ivCodigoQR: ImageView = findViewById(R.id.ivCodigoQR)
+        var etDatos : EditText = findViewById(R.id.etDatos)
+        var btnGenerar : Button = findViewById(R.id.btnGenerar)
+
+        btnGenerar.setOnClickListener(View.OnClickListener{
+            try{
+                var barcodeEncoder : BarcodeEncoder = BarcodeEncoder()
+                var bitmap : Bitmap = barcodeEncoder.encodeBitmap(
+                    etDatos.text.toString(),
+                    BarcodeFormat.QR_CODE,
+                    750,
+                    750
+                )
+                ivCodigoQR.setImageBitmap(bitmap)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        })
+    }
+}
